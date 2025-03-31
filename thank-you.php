bfb83023cde7c3f42ab8766ba3d794e18e04d79f
jnelbaylen <jnelbaylen@gmail.com>
2025-03-31 20:19:57 +0800

    last page

---
+++ thank-you.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Thank You</title>
+    <link rel="icon" href="./assets/icon.png">
+    <link rel="stylesheet" href="./src/thank-you.css">
+</head>
+<body>
+    
+
+    <div class="container">
+        <h1>Thank You for Your Submission</h1>
+        <p>Your information has been successfully recorded.</p>
+        <a href="./index.php" class="btn">Back to Form</a>
+    </div>
+
+</body>
+</html>
