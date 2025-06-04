012835d069b8ede771de8badfba0dd1fff181667
JohnnelBaylen <jnelbaylen@gmail.com>
2025-06-04 17:55:17 +0800

    chore: update folder name and change file path

--- src/index.php
+++ src/index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Student Information Form</title>
-    <link rel="icon" href="../assets/icon.png">
+    <link rel="icon" href="../public/icon.png">
     <link rel="stylesheet" href="../styles/style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css">
 </head>
--- src/thankyou.php
+++ src/thankyou.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Thank You</title>
-    <link rel="icon" href="../assets/icon.png">
+    <link rel="icon" href="../public/icon.png">
     <link rel="stylesheet" href="../styles/thankyou.css">
 </head>
 <body>
